baefa07882eccce3f4bf55f301fb9e9c54309dd5
Viggo Guerriero <viggo@viggomichael.com>
2025-05-21 13:51:11 -0400

    Add Terraform configuration for EC2 nginx

---
+++ infra/main.tf
@@ -0,0 +1,25 @@
+variable "region" {}
+variable "ami" {}
+variable "instance_type" {}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  user_data = <<-USERDATA
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y nginx
+              echo "Hello from Terraform" > /usr/share/nginx/html/index.html
+              systemctl enable nginx
+              systemctl start nginx
+              USERDATA
+
+  tags = {
+    Name = "terraform-ec2-nginx"
+  }
+}
